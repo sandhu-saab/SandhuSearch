e8646c457d6050d193a8403c1abbec73582f2ead
sandhu-saab <sandhusaab61291@gmail.com>
2026-01-20 08:28:06 +0530

    Fix string replacement for single quote in search

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -845,7 +845,7 @@ async def auto_filter(client, msg, spoll=False):
             search = search.lower()
             search = search.replace("-", " ")
             search = search.replace(":","")
-			search = search.replace("'","")
+            search = search.replace("'","")
             search = re.sub(r'\s+', ' ', search).strip()
             m=await message.reply_text(f'<b>Wait {message.from_user.mention} Searching Your Query: <i>{search}...</i></b>', reply_to_message_id=message.id)
             files, offset, total_results = await get_search_results(message.chat.id ,search, offset=0, filter=True)
